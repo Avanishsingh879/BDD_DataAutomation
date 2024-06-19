f1df34567fcacc1c6bd864a666a05ef74013a478
unknown <avanishsingh879@gmail.com>
2024-06-19 11:36:45 +0530

    Date: 19-6-24 Code updated

--- src/test/java/com/genpact/stepdefinition/DB_Testing2.java
+++ src/test/java/com/genpact/stepdefinition/DB_Testing2.java
@@ -11,21 +11,23 @@ public class DB_Testing2 {
 	
 	
 	
-	//Database connection with Selenium Date 16-6-2024
+	//Database connection with Selenium Date 19-6-2024
 	
 	public static void main(String args[]) throws ClassNotFoundException, SQLException {
 		
 		Class.forName("com.mysql.jdbc.Driver");
-		Connection conn=DriverManager.getConnection("jdbc:mysql://localhost:3306/campus", "root", "Mathura@!0809");
+		Connection con=DriverManager.getConnection("jdbc:mysql://localhost:3306/campus", "root", "Mathura@!0809");
 		
-		Statement smt=conn.createStatement();
+		Statement smt=con.createStatement();
 		ResultSet rs=smt.executeQuery("select * from campus.Item");
 		
 		while(rs.next()) {
 			
-			String fistName=rs.getString("FirstName");
-			System.out.println(fistName);
+			String firstname=rs.getString("FirstName");
+			System.out.println(firstname);
 		}
-	System.out.println("DataCase Connected");
+		
+		System.out.println("DataBase Connected");
+		
 	}
 }
